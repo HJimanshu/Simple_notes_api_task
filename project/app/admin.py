3666bd65632ce4c076a62cfcf41e7c6b08191138
himsoni99@gmail.com <himsoni99@gmail.com>
2026-04-20 20:12:09 +0530

    all done

--- project/app/admin.py
+++ project/app/admin.py
@@ -5,4 +5,5 @@ from .models import Note
 # Register your models here.
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at', 'is_complete')
+    list_filter = ('is_complete')
 admin.site.register(Note, NoteAdmin)
