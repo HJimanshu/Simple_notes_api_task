7199ba48717e8a11b68c545af4eb065be529609c
himsoni99@gmail.com <himsoni99@gmail.com>
2026-04-20 18:20:02 +0530

    third commit

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Create_Note
+from .views import Create_Note,NoteList
 
 urlpatterns = [
     path('create_note/', Create_Note.as_view(), name='create_note'),
+    path('notes/', NoteList.as_view(), name='note_list'),
 ]
+
+
--- project/app/views.py
+++ project/app/views.py
@@ -17,3 +17,31 @@ class Create_Note(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+class NoteList(APIView):
+    def get(self, request):
+        queryset = Note.objects.all()
+        
+        #filter by is_complete
+        is_complete = request.query_params.get('is_complete')
+        if is_complete is not None:
+            if is_complete.lower() == 'true':
+                queryset = queryset.filter(is_complete=True)
+            elif is_complete.lower() == 'false':
+                queryset = queryset.filter(is_complete=False)
+        
+        #search by title
+        search = request.query_params.get('search')
+        if search:
+            queryset = queryset.filter(title__icontains=search)
+        #order by created_at
+        ordering = request.query_params.get('ordering')
+        if ordering == 'created_at':
+            queryset = queryset.order_by('created_at')
+        else:
+            queryset = queryset.order_by('-created_at')  # default ordering is descending by created_at         
+                  
+        
+        serializer = NoteSerializer(queryset, many=True)
+        return Response(serializer.data)
